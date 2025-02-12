0b7dd229885d06cc1bfbb5305b34967132d147fe
WalterX95 <walter_1995@hotmail.it>
2025-02-12 16:38:13 +0100

    Terzo Esercizio (Conto Corrente) Caricato

---
+++ ConsoleApp1/Models/ContoCorrente.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class ContoCorrente
+    {
+        private int _idConto;
+        private string _nomeConto = string.Empty;
+        private string _cognomeConto = string.Empty;
+        private int _saldoCorrente;
+        private string _bankName = string.Empty;
+        private bool _isOpenCount = false;
+
+        public int IdConto { get { return _idConto; } set { _idConto = value; } }
+        public string NameConto { get { return _nomeConto; } set { _nomeConto = value; _cognomeConto = value; } }
+        public string CognomeConto { get { return _cognomeConto; } set { _cognomeConto = value; } }
+
+        public int SaldoCorrente { get { return _saldoCorrente; } set { _saldoCorrente = value; } }
+
+        public string BankName { get { return _bankName; } set { _bankName = value; } }
+
+        public bool OpenCount { get { return _isOpenCount;  } set { _isOpenCount = value; } }
+        public ContoCorrente(int nConto, string nameConto, string cognomeConto ,string bankName ,int saldo) {
+            _idConto = nConto;
+            _nomeConto = nameConto;
+            _cognomeConto = cognomeConto;
+            _bankName = bankName;
+            _saldoCorrente = saldo;
+        }
+        public int ApriConto(int idConto)
+        {
+
+            if (OpenCount == true)
+            {
+                Console.WriteLine("Il conto è già aperto per questo cliente");
+                return 0;
+            }
+
+            if (_saldoCorrente < 1000)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Impossibile Aprire Il conto Corrente hai meno di 1.000 euro ");
+                
+                
+            }
+            else if (_saldoCorrente > 1000)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Conto Aperto !!");
+                Console.ForegroundColor = ConsoleColor.White;
+                OpenCount = true;
+            }
+
+           
+
+            return idConto;
+        }
+
+        public bool VisualizzaContoCorrente()
+        {
+            if (OpenCount == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Impossibile visualizzare il conto Nome e id Conto non trovato");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("ID Conto : " + IdConto);
+                Console.WriteLine("Correntista :" + NameConto + " " + CognomeConto);
+                Console.WriteLine("Saldo Corrente :" + SaldoCorrente);
+                Console.WriteLine("Nome Banca : " + BankName);
+            }
+            
+
+            return OpenCount;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Primo Esercizio Creo la classe ContoCorrente
+using ConsoleApp1.Models;
+
+ContoCorrente myConto1 = new ContoCorrente(1122245, "Walter", "Antonelli", "Intesa San Paolo", 2000);
+ContoCorrente myCont2 = new ContoCorrente(3737622, "Pinko", "Pallino", "IsyBank Data", 500);
+
+myConto1.ApriConto(myConto1.IdConto);
+myConto1.ApriConto(myConto1.IdConto);
+
+myConto1.VisualizzaContoCorrente();
+
+myCont2.ApriConto(myCont2.IdConto);
+
+
+myCont2.VisualizzaContoCorrente();
